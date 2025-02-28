9ddc4ccead8db8fd809a2fea9e163dc706fe955e
AngelEduardoSanchezRamos <angelesr2631@gmail.com>
2025-02-28 11:11:45 -0600

    Create Ejemplo 1.6.cpp

---
+++ Practicas cpp/Ejemplo 1.6.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main ()
+{
+	cout << "Este programa representa numeros enteros y los mismos en orden inverso" << "\n";
+	int A,B,C,D;
+	cout << "Ingresa el primer numero: " << "\n";
+	cin >> A;
+	cout << "Ingresa el segundo numero: " << "\n";
+	cin >> B;
+	cout << "Ingresa el tercer numero: " << "\n";
+	cin >> C;
+	cout << "Ingresa el cuarto numero: " << "\n";
+	cin >> D;
+	cout << D << " , " << C << " , " << B << " , " << A;
+
+	return 0;
+}
